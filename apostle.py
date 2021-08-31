b094f910d045de642ac84eedf16ddcf52d7834e6
Drew Stackhouse <drewstckhs@gmail.com>
2021-08-30 21:40:15 -0400

    Update apostle.py

--- apostle.py
+++ apostle.py
@@ -1,124 +1,132 @@
 import os
 import re
-import sys
-import json
-import argparse
-import requests
 import threading
 import cloudscraper
 import pandas as pd
-from queue import Queue
 from bs4 import BeautifulSoup
 
+from bible import Bible
 
-def get_chapter(code, resource):
-  scraper = cloudscraper.create_scraper()
-  chapter = scraper.get(f"https://www.bible.com/bible/{code}/{resource}").content
-  soup = BeautifulSoup(chapter, 'html.parser')
-  verse_wrappers = [s for s in soup.find_all('span') if 'data-usfm' in s.attrs.keys()]
-  verse_nums = set([v.get('data-usfm') for v in verse_wrappers])
-  verse_spans = []
-  for v in verse_nums:
-    span_list = soup.find_all('span',attrs={'data-usfm':v})
-    all_verse_content = ""
-    for s in span_list:
-      individual_spans = s.find_all('span',attrs={'class':'content'})
-      content = ' '.join([i.text.strip() for i in individual_spans])
-      all_verse_content += content + ' '
-    verse_spans.append([v, all_verse_content.strip()])
-  verse_spans.sort(key=lambda pair: int(pair[0].split('.')[-1]))
-  verse_spans = {line[0]:line[1] for line in verse_spans}
-  return verse_spans
+class Apostle:
+  def __init__(self):
+    self.master = {}
 
-def worker(bible_obj):
-  while True:
-    try:
-      code = bible_obj.code
-      resource = bible_obj.queue.get()
-      chapter = get_chapter(code, resource)
-      bible_obj.update(chapter)
-      bible_obj.queue.task_done()
-    except:
-      print('An error occurred.')
+  def get_chapter(self, code, resource):
+    scraper = cloudscraper.create_scraper()
+    chapter = scraper.get(f"https://www.bible.com/bible/{code}/{resource}").content
+    soup = BeautifulSoup(chapter, 'html.parser')
+    verse_wrappers = [s for s in soup.find_all('span') if 'data-usfm' in s.attrs.keys()]
+    verse_nums = set([v.get('data-usfm') for v in verse_wrappers])
+    verse_spans = []
+    for v in verse_nums:
+      span_list = soup.find_all('span',attrs={'data-usfm':v})
+      all_verse_content = ""
+      for s in span_list:
+        individual_spans = s.find_all('span',attrs={'class':'content'})
+        content = ' '.join([i.text.strip() for i in individual_spans])
+        all_verse_content += content + ' '
+      verse_spans.append([v, all_verse_content.strip()])
+    verse_spans.sort(key=lambda pair: int(pair[0].split('.')[-1]))
+    verse_spans = {line[0]:line[1] for line in verse_spans}
+    return verse_spans
 
+  def worker(self, bible_obj):
+    while True:
+      try:
+        code = bible_obj.code
+        resource = bible_obj.queue.get()
+        chapter = self.get_chapter(code, resource)
+        bible_obj.update(chapter)
+        bible_obj.queue.task_done()
+      except:
+        print('An error occurred.')
 
-class Bible:
-  def __init__(self, code):
-    self.code = code
-    self.lock = threading.Lock()
-    self.verses = {}
-    self.queue = self.build_queue()
+  def get_multiple(self, source_target_dict):
+    source = source_target_dict['source']
+    target = source_target_dict['target']
+    source_plus_target = {code:None for code in source + target}
+    for code in source_plus_target:
+      if code not in self.master:
+        bible = Bible(code)
+        threads = []
+        n_threads = 50
+        for i in range(n_threads):
+          t = threading.Thread(target=self.worker, args=(bible, ), daemon=True)
+          threads.append(t)
+        for t in threads:
+          t.start()
+        bible.queue.join()
+        self.master[code] = bible.verses
+        print(f'{code} saved.')
+      else:
+        print(f'{code} already saved.')
+    print('All bibles saved.')
+    return self.master
 
-  def build_queue(self):
-    with open('index.json','r') as f:
-      index = json.loads(f.read())
+  def map_joins(self, outpath, source, target):
+    for s in source:
+      src = pd.DataFrame(list(self.master[s].items())).rename(columns={0:'verse',1:'src'}).set_index('verse')
+      for t in target:
+        tgt = pd.DataFrame(list(self.master[t].items())).rename(columns={0:'verse',1:'tgt'}).set_index('verse')
+        df = pd.merge(src, tgt, how='inner', on='verse')
+        df.src = df.src.str.replace('\n','')
+        df.tgt = df.tgt.str.replace('\n','')
+        df.to_csv(outpath, mode='a', sep='\t', header=None, index=None)
+        print(f'{s}_{t} added to {outpath}')
 
-    pages = []
-    for book in index:
-      num_verses = int(index.get(book))
-      book_codes = list(map(lambda x: f"{book}.{x}", list(range(1, num_verses+1))))
-      for b in book_codes:
-        pages.append(b)    
-    queue = Queue()
-    for p in pages:
-      queue.put(p)
-    return queue
-  
-  def update(self, new_verses):
-    with self.lock:
-      self.verses = {**self.verses, **new_verses}
+  def restrict_length(self, path, words=100):
+    new_path = f'{path.split(".txt")[0]}_filt.txt'
+    with open(new_path,'a') as outfile:
+      with open(path,'r') as infile:
+        for line in infile:
+          src, tgt = line.split('\t')
+          src_split = src.split(' ')
+          tgt_split = tgt.split(' ')
+          max_len = max([len(src_split), len(tgt_split)])
+          if max_len <= words:
+            outfile.write(line)
+      os.remove(path)
+      os.rename(new_path, path)
+      print('Length restriction complete.')
 
+  def build(self, outpath, source, target, expand=True):
+    source_target_dict = {'source': source, 'target': target}
+    self.get_multiple(source_target_dict)
+    self.map_joins(outpath, source, target)
+    if expand:
+      self.expand(outpath)
+    self.restrict_length(outpath)
 
-def get_multiple(source_target_dict):
-  source = source_target_dict['source']
-  target = source_target_dict['target']
-  master = {code:None for code in source + target}
-  for code in master:
-    bible = Bible(code)
-    threads = []
-    n_threads = 50
-    for i in range(n_threads):
-      t = threading.Thread(target=worker, args=(bible, ), daemon=True)
-      threads.append(t)
-    for t in threads:
-      t.start()
-    bible.queue.join()
-    master[code] = bible.verses
-    print(f'{code} saved.')
-  
-  print('All bibles saved.')
-  return master
-
-
-def map_joins(outpath, master_dict, source, target):
-  for s in source:
-    src = pd.DataFrame(list(master_dict[s].items())).rename(columns={0:'verse',1:'src'}).set_index('verse')
-    for t in target:
-      tgt = pd.DataFrame(list(master_dict[t].items())).rename(columns={0:'verse',1:'tgt'}).set_index('verse')
-      df = pd.merge(src, tgt, how='inner', on='verse')
-      df.src = df.src.str.replace('\n','')
-      df.tgt = df.tgt.str.replace('\n','')
-      df.to_csv(outpath, mode='a', sep='\t', header=None, index=None)
-      print(f'{s}_{t} added to {outpath}')
-
-
-def restrict_length(path, words=100):
-  new_path = f'{path.split(".txt")[0]}_filt.txt'
-  with open(new_path,'a') as outfile:
+  def expand(self, path):
     with open(path,'r') as infile:
-      for line in infile:
-        src, tgt = line.split('\t')
-        src_split = src.split(' ')
-        tgt_split = tgt.split(' ')
-        max_len = max([len(src_split), len(tgt_split)])
-        if max_len <= words:
-          outfile.write(line)
+      lines = infile.readlines()
     os.remove(path)
-    os.rename(new_path, path)
-    print('Length restriction complete.')
+
+    newlines = []
+    for line in lines:
+      en, ch = line.split('\t')
+      src_punc = re.findall('([\,\.\:\;\!\?\-])', en)
+      ch_punc = re.findall('([\,\.\:\;\!\?\-])', ch)
+      if src_punc == ch_punc:
+        src_split = re.split('([\,\.\:\;\!\?\-])', en)
+        ch_split = re.split('([\,\.\:\;\!\?\-])', ch)
+        en = src_split[0:-1:2]
+        ch = ch_split[0:-1:2]
+        src_tgt = list(zip(en, ch))
+        for line in src_tgt:
+          newlines.append('\t'.join(line).strip())
+      else:
+        newlines.append(line)
+
+    with open(path,'a+') as outfile:
+      for line in newlines:
+        if not line.endswith('\n'):
+          line += '\n'
+        outfile.write(line)
 
 
 if __name__ == '__main__':
+  import argparse
   parser = argparse.ArgumentParser()
   parser.add_argument('--output','-o', type=str, help="output filename")
   parser.add_argument('--source','-s', nargs='+', type=int, help="codes of source language texts")
@@ -129,7 +137,8 @@ if __name__ == '__main__':
   target = args.target
   outpath = args.output
 
+  a = Apostle()
   to_scrape = {'source':source, 'target':target}
-  master = get_multiple(to_scrape)
-  map_joins(outpath, master, source, target)
-  restrict_length(outpath)
+  a.get_multiple(to_scrape)
+  a.map_joins(outpath, source, target)
+  a.restrict_length(outpath)
